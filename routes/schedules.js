10bc626ddf22475d6802c07f3282202d97f93078
test_name <test_email>
2023-09-05 21:12:56 +0900

    UUID生成パッケージの導入、 データベースに予定を追加

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const ensurer = require('./authentication-ensurer');
+const { v4: uuidv4 } = require('uuid');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: [ 'query' ] });
 
 /* GET schedules listing. */
 router.get('/new', ensurer ,function(req, res, next) {
   res.render('new', { user: req.user});
 });
+
 router.get('/', function (req,res) {
   res.send('ここで予定の観覧ができるよ');
 })
-router.post('/', ensurer ,function (req, res, next) {
-  console.log(req.body); // TODO 予定と候補を保存する関数を実装
-  res.redirect('/schedules'); 
+
+router.post('/', ensurer , async function (req, res, next) {
+  console.log(req.body);
+  
+  const scheduleId = uuidv4();
+  console.log(scheduleId);
+  const updateAt = new Date();
+  const schedule = await prisma.schedule.create({
+    data: {
+      scheduleId: scheduleId,
+      scheduleName: req.body.scheduleName.slice(0,255) || '名称未設定', // 文字数制限
+      memo: req.body.memo,
+      createdBy: req.user,
+      updatedAt: updateAt
+    }
+  });
+
+  const candidateNames = req.body.candidates.split('\n').map((s) => s.trim()).filter((s) => s !== '');
+  const candidates = candidateNames.map((c) => ({
+    candidateName: c,
+    scheduleId: schedule.scheduleId
+  }));
+  await prisma.candidate.createMany({data: candidates})
+
+  res.redirect('/schedules/' + schedule.scheduleId); 
 })
 
 module.exports = router;
