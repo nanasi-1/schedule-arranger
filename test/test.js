678722968df097ef03a5ee6c4cd427efac5ec152
test_name <test_email>
2023-09-05 16:05:21 +0900

    02 終わり

---
+++ test/test.js
@@ -0,0 +1,43 @@
+'use strict'
+const request = require('supertest');
+const app = require('../app');
+const passportStub = require('passport-stub');
+
+describe('/login', () => {
+
+    beforeAll(() => {
+        passportStub.install(app);
+        passportStub.login({ username: 'test_user'});
+    })
+
+    test('ログインのためのフォームが含まれる', async () => {
+        await request(app).get('/login')
+
+            // 引数を二つ渡した場合はヘッダをテストする
+            .expect('Content-Type', 'text/html; charset=utf-8')
+
+            // 引数に正規表現を渡した場合はbodyにその文字列があるかをテストする
+            .expect(/<form/)
+
+            // 引数に数値を渡した場合はステータスコードをテストする
+            .expect(200)
+
+    })
+
+    test('ログイン時はユーザー名が表示される', async () => {
+        await request(app).get('/login')
+            .expect(/でログイン中/)
+            .expect(200)
+    })
+
+    test('ログアウト時はリダイレクトされる', async() => {
+        await request(app).get('/logout')
+            .expect('Location', '/')
+            .expect(302)
+    })
+
+    afterAll(() => {
+        passportStub.logout();
+        passportStub.uninstall();
+    })
+})
