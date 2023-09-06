529a81253a6b8c7a809c0a75b4eef688e4bc4518
test_name <test_email>
2023-09-06 10:48:01 +0900

    出欠データの取得

--- routes/schedules.js
+++ routes/schedules.js
@@ -11,11 +11,19 @@ router.get('/new', ensurer, function(req, res, next) {
 });
 
 router.get('/', ensurer, async function (req,res) {
+  /**
+   * @type {{
+   *  scheduleId: uuidv4, 
+   *  scheduleName: string,
+   *  memo: string,
+   *  createdBy: string,
+   *  updatedAt: data
+   * }}
+   */
   const schedules = await prisma.schedule.findMany({
     where: {createdBy: req.user}, // NOTE 一応置き換えしやすいようにした
     orderBy: { updatedAt: 'desc' }
   });
-  console.log(schedules);
   res.render('schedules', {schedules});
 })
 
@@ -46,6 +54,17 @@ router.post('/', ensurer , async function (req, res, next) {
 })
 
 router.get('/:scheduleId', ensurer, async (req,res,next) => {
+  /**
+   * 予定のデータ
+   * @type {{
+   *  scheduleId: uuidv4,
+   *  scheduleName: string,
+   *  memo: string,
+   *  createdBy: string,
+   *  updatedAt: data,
+   *  user: { userId: string, username: string}
+   * }}
+   */
   const schedule = await prisma.schedule.findUnique({
     where: {scheduleId: req.params.scheduleId},
     include: {
@@ -57,17 +76,90 @@ router.get('/:scheduleId', ensurer, async (req,res,next) => {
       }
     }
   })
-  if (schedule) {
+
+  if (schedule) { // 予定があるかどうかチェック
+
+    /**
+     * 候補のデータ
+     * @type {{ candidateId: number, candidateName: string, scheduleId: uuidv4}[]}
+     */
     const candidates = await prisma.candidate.findMany({
       where: { scheduleId: schedule.scheduleId },
       orderBy: { candidateId: 'asc' }
     });
+    
+    /**
+     * 出欠のデータ
+     * @type {Array<{user: {userId: string, username: string}}> | undefined}
+     */
+    const availabilities = await prisma.availability.findMany({
+      where: { scheduleId: schedule.scheduleId },
+      orderBy: {candidateId: 'asc'},
+      include: {
+        user: {
+          select: {
+            userId: true,
+            username: true
+          }
+        }
+      }
+    })
+    
+    /**
+     * 出欠MapMap
+     * key: userId, value: Map(key: candidateId, value: availability)
+     * @type {Object.<string, Object.<string,(0 | 1 | 2)>>}
+     */
+    const availabilityMapMap = new Map();
+    availabilities.forEach(ava => {
+      const map = availabilityMapMap.get(ava.user.userId) || new Map();
+      map.set(ava.candidateId, ava.availability);
+      availabilityMapMap.set(a.user.userId, map)
+    });
+    
+    /**
+     * ユーザ情報のマップ
+     * key: userId, value: User
+     * @type {Object.<string, {isSelf: boolean, userId: string, username: string}>}
+     */
+    const userMap = new Map();
+    userMap.set(req.user, {
+      isSelf: true,
+      userId: req.user,
+      username: req.user
+    });
+
+    availabilities.forEach(a => {
+      userMap.set(req.user, {
+        isSelf: req.user === a.user.userId, // 地震であるかどうかを示す真偽値
+        userId: a.user.userId,
+        username: a.user.username
+      });
+    });
+
+    // 全ユーザ、全候補で二重ループしてそれぞれの出欠の値がない場合には、「欠席」を設定する
+    /**
+     * ユーザ情報が入った配列
+     * @type {{isSelf: boolean, userId: string, username: string}[]}
+     */
+    const users = Array.from(userMap.values());
+    users.forEach((u) => {
+      candidates.forEach((c) => {
+        const map = availabilityMapMap.get(u.userId) || new Map();
+        const a = map.get(c.candidateId) || 0; // デフォルト値は 0 を使用
+        map.set(c.candidateId, a);
+        availabilityMapMap.set(u.userId, map);
+      });
+    });
+
     res.render('schedule', {
       user: req.user,
       schedule: schedule,
       candidates: candidates,
-      users: [ req.user ]
+      users: users,
+      availabilityMapMap: availabilityMapMap
     });
+
   } else {
     const err = new Error('指定された予定は見つかりません');
     err.status = 404;
